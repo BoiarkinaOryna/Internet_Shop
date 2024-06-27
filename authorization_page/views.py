b57dea5f1215eafd958a0bf901f6c921225f5546
BoiarkinaOryna <boyarkina.ar@gmail.com>
2024-06-27 18:47:32 +0300

    readme improving

--- authorization_page/views.py
+++ authorization_page/views.py
@@ -6,21 +6,10 @@ from flask_login import current_user
 from registration_page.views import is_registered
 
 def render_authorization_page():
-    # print("User.query:", User.query.all())
     if flask.request.method == "POST":
-        # if flask_login.current_user.is_authenticated:
-            
-
-            # print("Ви вже авторизовані")
-            # return "Ви вже авторизовані"
-        # else:
         for user in User.query.filter_by(name = flask.request.form.get("name")):
             if user.password == flask.request.form['password']:
                 flask_login.login_user(user)
                 return flask.redirect("/")
-        # user_name = None
-        # if flask_login.current_user.is_authenticated:
-        #     user_name = flask_login.current_user.name
                     
-                
     return flask.render_template(template_name_or_list = "authorization.html", is_registered = is_registered)
